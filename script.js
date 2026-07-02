11365c9d971e8644ec2bf19b2453bdfa7f6e99e4
Anjali-Dhangar <anjalidhangar786@gmail.com>
2026-07-02 06:27:00 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-window.onload = function () {
-  alert("Welcome to My Portfolio Website !")
-};
-const links = 
-  Document.querySelectorAll('a[href^="#"]');
-links.forEach(link => {
-  link.addEventListener("click", function (e)
-                        
